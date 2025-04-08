c722f6cedacc9dcf5380a30c7fac2c8fce8270da
maurexfontys <m.waelen@student.fontys.nl>
2025-04-08 11:35:43 +0200

    Update entrypoint.sh to use yarn start

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -3,8 +3,4 @@
 # Ensure we kill the service if migration fails
 set -e
 
-# Wait for any services if needed
-# Example: wait-for-it.sh database:5432 -t 60
-
-# Start the application
-node dist/index.js
+yarn start
