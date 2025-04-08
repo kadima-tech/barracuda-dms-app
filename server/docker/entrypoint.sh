64fde952c3a3512b4a704704bd05229ec6b83e0f
maurexfontys <m.waelen@student.fontys.nl>
2025-04-08 11:32:03 +0200

    Add Docker entrypoint script

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -3,4 +3,8 @@
 # Ensure we kill the service if migration fails
 set -e
 
-yarn start
+# Wait for any services if needed
+# Example: wait-for-it.sh database:5432 -t 60
+
+# Start the application
+node dist/index.js
